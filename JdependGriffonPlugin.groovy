ae6423fc1e7d1a2f1fa55f6687eaac4cb0ecf96d
aalmiray <aalmiray@gmail.com>
2012-06-07 22:41:40 +0200

    Sync with Griffon 1.0.0

--- JdependGriffonPlugin.groovy
+++ JdependGriffonPlugin.groovy
@@ -19,9 +19,9 @@
  */
 class JdependGriffonPlugin {
     // the plugin version
-    String version = '0.4'
+    String version = '0.5'
     // the version or versions of Griffon the plugin is designed for
-    String griffonVersion = '0.9.5 > *'
+    String griffonVersion = '1.0.0 > *'
     // the other plugins this plugin depends on
     Map dependsOn = [:]
     // resources that are included in plugin packaging
@@ -39,6 +39,8 @@ class JdependGriffonPlugin {
     String documentation = ''
     // URL where source can be found
     String source = 'https://github.com/griffon/griffon-jdepend-plugin'
+    // Install as a framework plugin
+    boolean framework = true
 
     List authors = [
             [
